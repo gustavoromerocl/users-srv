7efc11a3e7ffd773b649e254ab036a849b864504
gustavoromerocl <gandresrp@gmail.com>
2024-10-28 00:07:11 -0300

    feat: refactor customer user detail service

--- src/main/java/com/duocuc/users_srv/security/CustomUserDetailsService.java
+++ src/main/java/com/duocuc/users_srv/security/CustomUserDetailsService.java
@@ -1,9 +1,15 @@
 package com.duocuc.users_srv.security;
 
 import com.duocuc.users_srv.model.User;
+import com.duocuc.users_srv.repository.UserRepository;
 import com.duocuc.users_srv.service.UserService;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -13,12 +19,21 @@ import org.springframework.stereotype.Service;
 public class CustomUserDetailsService implements UserDetailsService {
 
   @Autowired
-  private UserService userService;
+  private UserRepository userRepository;
 
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    User user = userService.findByUsername(username)
-        .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
-    return UserPrincipal.create(user);
+    User user = userRepository.findByUsername(username)
+        .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+    // Convertir roles a GrantedAuthority
+    Set<GrantedAuthority> authorities = user.getRoles().stream()
+        .map(role -> new SimpleGrantedAuthority(role.getCode()))
+        .collect(Collectors.toSet());
+
+    return new org.springframework.security.core.userdetails.User(
+        user.getUsername(),
+        user.getPassword(),
+        authorities);
   }
 }
