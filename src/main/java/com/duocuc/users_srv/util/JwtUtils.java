4b1b5a05dcfe9b4d1e14960306f1960a35999a12
gustavoromerocl <gandresrp@gmail.com>
2024-10-28 00:05:51 -0300

    refactor: jwt util service has been refactored

--- src/main/java/com/duocuc/users_srv/util/JwtUtils.java
+++ src/main/java/com/duocuc/users_srv/util/JwtUtils.java
@@ -3,12 +3,10 @@ package com.duocuc.users_srv.util;
 import com.duocuc.users_srv.config.JwtConfig;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
-
-import javax.crypto.spec.SecretKeySpec;
+import jakarta.servlet.http.HttpServletRequest;
 import java.security.Key;
 import java.util.Base64;
 import java.util.Date;
@@ -25,15 +23,22 @@ public class JwtUtils {
     return Keys.hmacShaKeyFor(keyBytes); // Usa una clave de al menos 512 bits
   }
 
+  // Generar el token JWT
   public String generateJwtToken(Authentication authentication) {
+    String roles = authentication.getAuthorities().stream()
+        .map(grantedAuthority -> grantedAuthority.getAuthority())
+        .reduce((a, b) -> a + "," + b).orElse("");
+
     return Jwts.builder()
         .setSubject(authentication.getName())
+        .claim("roles", roles) // Agregar roles al token
         .setIssuedAt(new Date())
         .setExpiration(new Date((new Date()).getTime() + jwtConfig.getExpirationMs()))
         .signWith(getSigningKey(), SignatureAlgorithm.HS512)
         .compact();
   }
 
+  // Validar el token JWT
   public boolean validateToken(String token) {
     try {
       Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(token);
@@ -43,12 +48,21 @@ public class JwtUtils {
     }
   }
 
-  public String getUsernameFromToken(String token) {
+  // Obtener Claims desde el token JWT
+  public Claims getClaimsFromToken(String token) {
     return Jwts.parserBuilder()
         .setSigningKey(getSigningKey())
         .build()
         .parseClaimsJws(token)
-        .getBody()
-        .getSubject();
+        .getBody();
+  }
+
+  // Obtener el token JWT desde la solicitud
+  public String getJwtFromRequest(HttpServletRequest request) {
+    String bearerToken = request.getHeader("Authorization");
+    if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+      return bearerToken.substring(7); // Quita "Bearer " para obtener solo el token
+    }
+    return null;
   }
 }
