0d41b8d0e3a8cd832a581149820bbf65b089c8c7
gustavoromerocl <gandresrp@gmail.com>
2024-12-01 20:17:23 -0300

    refactor: set email on login service

--- src/main/java/com/duocuc/users_srv/model/User.java
+++ src/main/java/com/duocuc/users_srv/model/User.java
@@ -1,8 +1,19 @@
 package com.duocuc.users_srv.model;
 
-import jakarta.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "users")
@@ -12,7 +23,7 @@ public class User {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @Column(unique = true, nullable = false)
+  @Column(nullable = false)
   private String username;
 
   @Column(nullable = false)
